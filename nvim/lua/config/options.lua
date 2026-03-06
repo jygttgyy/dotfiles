c17ee580b9430e4dd11671f99e542780e3005fc3
jygttgyy <gabrieljoris1402@gmail.com>
2026-03-06 17:36:55 +0100

    e

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -24,7 +24,14 @@ vim.opt.hlsearch = false            -- do not highlight matches
 vim.opt.ignorecase = true           -- ignore case in searches by default
 vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
 
-vim.diagnostic.config({ virtual_lines = true })
+vim.diagnostic.config({
+    virtual_text = false,
+    virtual_lines = true,
+    signs = true,
+    underline = true,
+    severity_sort = true,
+})
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
 
